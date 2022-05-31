320acca20da9e3f13cbb60632216f2ac1d859a00
John Shen <qpq9sk@virginia.edu>
2022-05-31 18:04:21 -0500

    MRO

---
+++ MRO.py
@@ -0,0 +1,20 @@
+class TimeEncode(torch.nn.Module):
+  # Time Encoding proposed by TGAT
+  def __init__(self, dimension):
+    super(TimeEncode, self).__init__()
+
+    self.dimension = dimension
+    self.w = torch.nn.Linear(1, dimension)
+
+    self.w.weight = torch.nn.Parameter((torch.from_numpy(1 / 10 ** np.linspace(0, 9, dimension)))
+                                       .float().reshape(dimension, -1))
+    self.w.bias = torch.nn.Parameter(torch.zeros(dimension).float())
+    
+    
+    
+#Rewrite
+class TimeEncode(torch.nn.Module):
+    def__init(self,dimension):
+        nn.Module.__init__(self,dimension)
+        self.dimension=dimension
+        ...
